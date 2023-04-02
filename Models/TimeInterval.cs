039a96bb2dedbf534ca861e3c99209e3c99ee613
toporoviv <ya.toporow@yandex.ru>
2023-04-02 23:21:09 +0300

    ViewModel
    Добавлены модели

---
+++ Models/TimeInterval.cs
@@ -0,0 +1,9 @@
+﻿namespace WebRunApplication.Models
+{
+    public class TimeInterval
+    {
+        public DateTime Begin { get; set; }
+
+        public DateTime End { get; set; }
+    }
+}
---
+++ Models/TrainingPdfViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebRunApplication.Models
+{
+    public class TrainingPdfViewModel
+    {
+        public string Title { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
