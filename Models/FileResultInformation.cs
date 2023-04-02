cabc3a6cf728282171d202cc02f67c01da07c4ed
toporoviv <ya.toporow@yandex.ru>
2023-04-02 23:45:19 +0300

    Модели
    Добавлены новые модели

---
+++ Models/FileResultInformation.cs
@@ -0,0 +1,11 @@
+﻿namespace WebRunApplication.Models
+{
+    public class FileResultInformation
+    {
+        public byte[] Data { get; set; }
+
+        public string ContentType { get; set; }
+
+        public string FileName { get; set; }
+    }
+}
---
+++ Models/TrainingTotalInformationViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebRunApplication.Models
+{
+    public class TrainingTotalInformationViewModel
+    {
+        public string Title { get; set; }
+
+        public TimeSpan Duration { get; set; }
+    }
+}
