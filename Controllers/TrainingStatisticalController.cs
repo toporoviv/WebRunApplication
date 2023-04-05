826071ca456f1baf920c064113d87f2cf01c5fe6
toporoviv <ya.toporow@yandex.ru>
2023-04-05 18:02:04 +0300

    Контроллер
    Добавлен контроллер для работы со статистикой

---
+++ Controllers/TrainingStatisticalController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using WebRunApplication.DataEntity;
+using WebRunApplication.Models;
+using WebRunApplication.Services.Interfaces;
+
+namespace WebRunApplication.Controllers
+{
+    [Authorize]
+    public class TrainingStatisticalController : Controller
+    {
+        private readonly ITrainingStatisticalService _trainingStatisticalService;
+
+        public TrainingStatisticalController(ITrainingStatisticalService trainingStatisticalService)
+        {
+            _trainingStatisticalService = trainingStatisticalService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var responseMailingCount = await _trainingStatisticalService.GetTotalMailingCount(User.Identity.Name);
+
+            if (responseMailingCount.StatusCode != Enums.StatusCode.OK)
+            {
+                ModelState.AddModelError("", responseMailingCount.Description);
+                return View();
+            }
+
+            ViewBag.TotalMailingCount = responseMailingCount.Data;
+
+            var responseTrainingDuration = await _trainingStatisticalService.GetTotalTrainingDuration(User.Identity.Name);
+
+            if (responseTrainingDuration.StatusCode != Enums.StatusCode.OK)
+            {
+                ModelState.AddModelError("", responseTrainingDuration.Description);
+                return View();
+            }
+
+            ViewBag.TotalTrainingDuration = responseTrainingDuration.Data;
+
+            var responseTrainingCount = await _trainingStatisticalService.GetTotalTrainingCount(User.Identity.Name);
+
+            if (responseTrainingCount.StatusCode != Enums.StatusCode.OK)
+            {
+                ModelState.AddModelError("", responseTrainingCount.Description);
+                return View();
+            }
+
+            ViewBag.TotalTrainingCount = responseTrainingCount.Data;
+
+            var responseTrainingDayDuration = await _trainingStatisticalService.GetTotalTrainingDayDuration(User.Identity.Name);
+
+            if (responseTrainingDayDuration.StatusCode != Enums.StatusCode.OK)
+            {
+                ModelState.AddModelError("", responseTrainingDayDuration.Description);
+                return View();
+            }
+
+            ViewBag.TotalTrainingDayDuration = responseTrainingDayDuration.Data;
+
+            return View();
+        }
+
+    }
+}
