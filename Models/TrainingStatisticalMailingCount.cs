e633af5161ec73c44661d12175acdcdb854bb589
toporoviv <ya.toporow@yandex.ru>
2023-04-05 18:04:56 +0300

    ViewModel
    ViewModel

---
+++ Models/TrainingStatisticalMailingCount.cs
@@ -0,0 +1,9 @@
+﻿namespace WebRunApplication.Models
+{
+    public class TrainingStatisticalMailingCount
+    {
+        public DateTime Date { get; set; }
+
+        public uint Count { get; set; }
+    }
+}
---
+++ Models/TrainingStatisticalTotalDurationView.cs
@@ -0,0 +1,9 @@
+﻿namespace WebRunApplication.Models
+{
+    public class TrainingStatisticalTotalDurationView
+    {
+        public DateTime Date { get; set; }
+
+        public TimeSpan TotalDuration { get; set; }
+    }
+}
