f9716d3dee79c1bb1f83f77cf3a6c61886cd2016
toporoviv <ya.toporow@yandex.ru>
2023-04-14 20:54:22 +0300

    ViewModel
    ViewModel

---
+++ Models/MovingAverage.cs
@@ -0,0 +1,15 @@
+﻿namespace WebRunApplication.Models
+{
+    public class MovingAverage
+    {
+        public uint Id { get; set; }
+
+        public double? Value { get; set; }
+
+        public double? L3 { get; set; }
+
+        public double? L5 { get; set; }
+
+        public double? L7 { get; set; }
+    }
+}
